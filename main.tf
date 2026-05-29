e688d1a637064c89637655b804a2d0819b58859d
jairo <jairobuitrago3@gmail.com>
2026-05-28 22:15:39 -0500

    adding a new resource using vcs

--- main.tf
+++ main.tf
@@ -10,3 +10,11 @@ resource "azurerm_storage_account" "staccdemo" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
+
+resource "azurerm_virtual_network" "vnetdemo" {
+  name                = "vnetdemo"
+  address_space       = [ "10.10.0.0/16" ]
+  resource_group_name = azurerm_resource_group.rg-demo.name
+  location            = azurerm_resource_group.rg-demo.location
+
+}
