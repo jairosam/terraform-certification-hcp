79a2b2b57907e3fa36ca1353822542cd985b84e6
jairo <jairobuitrago3@gmail.com>
2026-05-29 13:51:37 -0500

    testing variable sets

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 resource "azurerm_resource_group" "rg-demo" {
-  name     = "rg-demo"
+  name     = var.resource-group-name
   location = "eastus"
 }
 
 resource "azurerm_storage_account" "staccdemo" {
-  name                     = "stacdemohcpjairotest"
+  name                     = var.storageaccountname
   resource_group_name      = azurerm_resource_group.rg-demo.name
   location                 = azurerm_resource_group.rg-demo.location
   account_tier             = "Standard"
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "resource-group-name" {
+  type        = string
+  description = "Name of the resource group to create"
+}
+
+variable "storageaccountname" {
+  type        = string
+  description = "Name of the storage account to create"
+}
